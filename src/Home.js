d27010f8cc1e94aff469623d4078abf70ebbc00c
zakariyahya <yazakkars@gmail.com>
2022-09-19 12:26:37 +0700

    create react app

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+ 
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Home Page</h2>
+        <p>This Is Home Page</p>
+      </div>
+    );
+  }
+}
+ 
+export default Home;
