b5cdadbdecca016445ffdc9be80e62374c80a8e0
zakariyahya <yazakkars@gmail.com>
2022-09-21 10:28:45 +0700

    react faker comment

--- src/reactFakerComment.js
+++ src/reactFakerComment.js
@@ -1,14 +1,16 @@
 import React from "react"
 import ReactDom from "react-dom/client"
+import { faker } from '@faker-js/faker';
 
 const element = document.getElementById("root")
 
 const root = ReactDom.createRoot(element)
 
-const User = ({nama, pekerjaan})=>{
+const User = ({name, comment})=>{
   return (
       <div>
-          <h4>Nama : {nama} <br />Pekerjaan : {pekerjaan}</h4>      
+          {name}    
+          {comment}    
       </div>
   )
 }
@@ -18,20 +20,29 @@ const App = () => {
         <div className="ui container comments">
             <div className="comment">
                 <a href="/" className="avatar">
-                    <img alt="avatar" />
+                    <img alt="avatar" src={faker.image.avatar()} />
                 </a>
+                <div className="content">
+                  <a href="/" className="author">
+                  <User name={faker.name.firstName()}/>
+                  </a>
+                  <div className="metadata">
+                      <span className="date"> Today at {new Date().toLocaleTimeString()} </span>
+                  </div>
+                  <div className="text"> <User comment={faker.lorem.lines()}/></div>
+                </div>
             </div>
-            <div className="content">
-                <a href="/" className="author">
-                <User nama="Yahya" />
-                </a>
-            </div>
-            <div className="metadata">
-                <span className="date">{new Date().toLocaleTimeString()}</span>
-            </div>
-            <div className="text">Test Comment</div>
         </div>
     )
 }
 
-root.render(<App />)
+const Func = () =>{
+  return (
+    <div>
+      <App />
+      <App />
+      <App />
+    </div>
+  )
+}
+root.render(<Func />)
